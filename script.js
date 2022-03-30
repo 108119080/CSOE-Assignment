8580a7cf699acf1f9459cce8ec986d4a170b87e2
108119080 <63252651+108119080@users.noreply.github.com>
2022-03-30 15:36:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let fact=document.getElementById("fact")
+let facttext=document.getElementById("factText")
+let numberinput=document.getElementById("numberinput")
+
+numberinput.addEventListener("input",getFact)
+
+
+function getFact(){
+    let number=numberinput.value
+
+    //AJAX request for fetching the details from the Numbers API
+    let xhr=new XMLHttpRequest()
+    xhr.open("GET","http://numbersapi.com/"+number)
+    xhr.onload=function(){
+        
+    if(this.status==200 & number !=""){
+        fact.style.display="block"
+        facttext.innerText=this.responseText
+    }
+    window.scrollTo(0,document.body.scrollHeight);
+    }
+    xhr.send()
+    
+}
